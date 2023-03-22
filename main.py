38341a7f5dc9d79388d07cc05917cadfee8ccfb3
Serwerownia <92176171+Serwerownia@users.noreply.github.com>
2023-03-22 21:02:59 +0100

    .

--- main.py
+++ main.py
@@ -12,10 +12,10 @@ import time
 
 #res = pyautogui.locateCenterOnScreen("targets/mob.png")
 #print(pyscreeze.locateCenterOnScreen("swierszcz.png"))
-check = pyautogui.locateOnScreen("targets/test.png") 
-screen = pyautogui.screenshot('zdj.png',region=(250,205,875,510))
-time.sleep(10)
-screen1 = pyautogui.screenshot('zdj1.png',region=(250,205,875,510))
+#check = pyautogui.locateOnScreen("targets/test.png")
+screen = pyautogui.screenshot('zdj.png',region=(250,205,1420,760))
+time.sleep(32)
+screen1 = pyautogui.screenshot('zdj1.png',region=(250,205,1420,760))
 
 img1 = cv2.imread('zdj.png')
 img2 = cv2.imread('zdj1.png')
@@ -27,51 +27,41 @@ def mse(img1, img2):
    diff = cv2.subtract(img1, img2)
    err = np.sum(diff**2)
    mse = err/(float(h*w))
-   return mse
+   return mse, diff
 
- = mse(img1, img2)
-#print("Image matching Error between the two images:",error)
+error, diff = mse(img1, img2)
 
-cv2.imshow("difference", diff)
-#cv2.waitKey(0)
+diff = cv2.Canny(diff, 127, 127 * 2)
+
+contours, _ = cv2.findContours(diff, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+contours_poly = [None]*len(contours)
+boundRect = [None]*len(contours)
+centers = [None]*len(contours)
+radius = [None]*len(contours)
+for i, c in enumerate(contours):
+    contours_poly[i] = cv2.approxPolyDP(c, 3, True)
+    boundRect[i] = cv2.boundingRect(contours_poly[i])
+    centers[i], radius[i] = cv2.minEnclosingCircle(contours_poly[i])
+
+print("Image matching Error between the two images:",error)
+
+#cv2.circle(diff,(int(centers[0][0]), int(centers[0][1])), 10 , (255,0,0) ,-1)
+
+pyautogui.moveTo(int(centers[0][0]+250), int(centers[0][1]+200))
+time.sleep(1)
+zuk = pyautogui.locateCenterOnScreen("zuk.png")
+time.sleep(1)
+if(zuk != None):
+    pyautogui.click()
+    time.sleep(1)
+    pyautogui.hotkey("f")
+    time.sleep(1)
+    pyautogui.hotkey("z")
+
+
+#cv2.imshow("difference", diff)
+cv2.waitKey(0)
 #cv2.destroyAllWindows()
-
-"""
-if(screen == screen1):
-    print("takie same")
-else:
-    print("rozne")
-
-
-if (res != None):
-    pyautogui.moveTo(res)
-    time.sleep(2)
-    pyautogui.click()
-    time.sleep(5)
-    pyautogui.hotkey("f")
-    time.sleep(5)
-    pyautogui.hotkey("z")
-elif (res1 != None):
-    pyautogui.moveTo(res1)
-    time.sleep(2)
-    pyautogui.click()
-    time.sleep(5)
-    pyautogui.hotkey("f")
-    time.sleep(5)
-    pyautogui.hotkey("z")
-else:
-    print("gagri")
-"""
-
-#if(x == None & y == None):
-#    exit("could not find mb")
-
-#pyscreeze.click(x,y)
-"""
-time.sleep(2)
-pyautogui.hotkey("f")
-time.sleep(2)
-pyautogui.hotkey("z")
-"""
 
 
